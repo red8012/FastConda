e04de27c1cbb4a1e9a96f62b96ce048e33d99e01
Yu-Ann Chen <red8012@gmail.com>
2017-12-20 22:58:47 +0800

    add a script to aggregate djangobench result

---
+++ aggregateBenchmarkResult.py
@@ -0,0 +1,24 @@
+TARGETS = ['alpine', 'optimizedconda', 'official', 'revsys', 'miniconda', 'intel']
+N_ROUNDS = 1
+
+result1 = {target: [] for target in TARGETS}
+result2 = {target: [] for target in TARGETS}
+
+for target in TARGETS:
+    with open(f'{target}-1.txt') as f:
+        django1_time = 0.
+        django2_time = 0.
+        for line in f:
+            if line.startswith('Avg'):
+                split = line.split()
+                django1_time += float(split[1])
+                django2_time += float(split[3][:-1])
+        result1[target].append(django1_time)
+        result2[target].append(django2_time)
+
+with open('result1.txt', 'w') as f:
+    for target in TARGETS:
+        f.write(f'{target},{",".join(str(i) for i in result1[target])}')
+with open('result2.txt', 'w') as f:
+    for target in TARGETS:
+        f.write(f'{target},{",".join(str(i) for i in result2[target])}')
